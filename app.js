2ab4ded1b9d755b0c4cf788b9dfd30ba2d4d56fe
Christopher Brown <christopher.p.brown@gmail.com>
2016-10-04 17:20:26 -0600

    headerImg

--- app.js
+++ app.js
@@ -5,53 +5,92 @@ var app = angular.module('yogasite', ['ui.router'])
     url: '',
     templateUrl: '/partials/about.html',
     resolve: {
-      header: function() {
-        return '/img/001.jpg';
-      },
-      title: function() {
-        return 'Mel Torgusen';
+      headerImg: function(){
+        return '/img/003.jpg';
       }
     },
     controller: 'SiteCtrl',
-    controllerAs: 'mt'
+    controllerAs: 'yoga'
   }
 
   var aboutState = {
     name: 'about',
     url: '/about',
-    templateUrl: '/partials/about.html'
+    templateUrl: '/partials/about.html',
+    resolve: {
+      headerImg: function(){
+        return 'about';
+      }
+    },
+    controller: 'SiteCtrl',
+    controllerAs: 'yoga'
   }
 
   var classesState = {
     name: 'classes',
     url: '/classes',
-    templateUrl: '/partials/classes.html'
+    templateUrl: '/partials/classes.html',
+    resolve: {
+      headerImg: function(){
+        return 'classes';
+      }
+    },
+    controller: 'SiteCtrl',
+    controllerAs: 'yoga'
   }
 
   var locationsState = {
     name: 'classes.locations',
     parent: 'classes',
     url: '/locations',
-    templateUrl: '/partials/locations.html'
+    templateUrl: '/partials/locations.html',
+    resolve: {
+      headerImg: function(){
+        return 'classes';
+      }
+    },
+    controller: 'SiteCtrl',
+    controllerAs: 'yoga'
   }
 
   var calendarState = {
     name: 'classes.calendar',
     parent: 'classes',
     url: '/calendar',
-    templateUrl: '/partials/calendar.html'
+    templateUrl: '/partials/calendar.html',
+    resolve: {
+      headerImg: function(){
+        return '/img/003.jpg';
+      }
+    },
+    controller: 'SiteCtrl',
+    controllerAs: 'yoga'
   }
 
   var offTheMatState = {
     name: 'off-the-mat',
     url: '/off-the-mat',
-    templateUrl: '/partials/offmat.html'
+    templateUrl: '/partials/offmat.html',
+    resolve: {
+      headerImg: function(){
+        return 'offmat';
+      }
+    },
+    controller: 'SiteCtrl',
+    controllerAs: 'yoga'
   }
 
   var contactState = {
     name: 'contact',
     url: '/contact',
-    templateUrl: '/partials/contact.html'
+    templateUrl: '/partials/contact.html',
+    resolve: {
+      headerImg: function(){
+        return 'contact';
+      }
+    },
+    controller: 'SiteCtrl',
+    controllerAs: 'yoga'
   }
 
   $stateProvider.state(homeState);
@@ -62,9 +101,9 @@ var app = angular.module('yogasite', ['ui.router'])
   $stateProvider.state(offTheMatState);
   $stateProvider.state(contactState);
 })
-.controller('SiteCtrl', ['$scope', 'title', function($scope, title){
-  // NOTE this is only passed to the state?
-  console.log(title);
-  this.title = title;
+.controller('SiteCtrl', ['$scope', 'headerImg', function($scope, headerImg){
+  this.title = 'Mel Torgusen';
+  console.log(headerImg);
+  this.headerImg = headerImg;
 }]);
 
